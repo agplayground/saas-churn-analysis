07b12c89e3607250fb470dc64c37284266a3e180
agplayground <Augis998@gmail.com>
2026-04-14 11:08:07 +0300

    Create 01_funnel_summary.sql

---
+++ 01_funnel_summary.sql
@@ -0,0 +1,37 @@
+-- Identifying active and churned users
+WITH funnel_base AS (
+SELECT 
+  COUNT(a.account_id) AS signups,
+  COUNT(CASE WHEN churn_flag=false THEN a.account_id END) AS active,
+  COUNT(CASE WHEN churn_flag=true THEN a.account_id END) AS churned
+FROM accounts a
+),
+-- Ranking subscriptions to be able to pull only the latest one for churned MRR/ARR calculations, ignoring trials
+last_sub AS (
+  SELECT
+    account_id,
+    mrr_amount,
+    ROW_NUMBER() OVER (PARTITION BY account_id ORDER BY start_date DESC) AS rn
+  FROM subscriptions
+  WHERE is_trial = false
+),
+-- Calculating churned revenue
+churned_revenue AS (
+SELECT
+  ROUND(SUM(ls.mrr_amount), 2) AS lost_mrr,
+  ROUND(SUM(ls.mrr_amount) * 12, 2) AS lost_arr
+FROM accounts a
+JOIN last_sub ls ON a.account_id = ls.account_id
+WHERE a.churn_flag = true
+  AND ls.rn = 1
+)
+-- Combining funnel and churn metrics to measure churn impact
+SELECT 
+  signups,
+  active,
+  churned,
+  churned / signups AS churn_rate,
+  lost_mrr,
+  lost_arr
+FROM funnel_base
+CROSS JOIN churned_revenue
